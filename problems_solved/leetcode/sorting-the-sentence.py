538e1c07408a13651a53e89652f3f69791f85fa0
yayne30 <101341804+yayne30@users.noreply.github.com>
2024-03-11 20:19:37 +0300

    Add solution for Sorting the Sentence

--- problems_solved/leetcode/sorting-the-sentence.py
+++ problems_solved/leetcode/sorting-the-sentence.py
@@ -1,23 +1,38 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
-        
-        cnt = Counter(s)
-        str_array= [''] *( cnt[' '] + 2)
-        
-        temp = ''
+
+        s_splited = s.split(' ')
         ans = ''
+        s_splited.sort(key = lambda x : x[-1])
+        cnt = Counter(s)
 
-        for i in s:
-            if not i.isdigit() and i != ' ':
-                temp += i
-            elif i.isdigit():
-                str_array[int(i)]= temp
-                temp = ''
-                
-        for i in range( 1, len(str_array)):
-            ans += str_array[i]
-            if i != len(str_array) - 1:
+    
+        for i in s_splited:
+            
+            print()
+            ans += i[:-1]
+            if int(i[-1]) < len(s_splited):
                 ans += ' '
-
         return ans
+
+
+        
+        # cnt = Counter(s)
+        # str_array= [''] *( cnt[' '] + 2)
+        # temp = ''
+        # ans = ''
+
+        # for i in s:
+        #     if not i.isdigit() and i != ' ':
+        #         temp += i
+        #     elif i.isdigit():
+        #         str_array[int(i)]= temp
+        #         temp = ''
+
+        # for i in range( 1, len(str_array)):
+        #     ans += str_array[i]
+        #     if i != len(str_array) - 1:
+        #         ans += ' '
+
+        # return ans
         
